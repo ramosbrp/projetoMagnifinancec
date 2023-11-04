85b773f5cbb8b0b105803f6c4d9f7e4bbfcda0a5
Bruno <bruno.rp45@gmail.com>
2023-11-04 17:44:02 -0300

    Adicionando models

---
+++ backend/Models/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyUniversityAPI.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string NumeroMatricula { get; set; }
+        // Supondo que um aluno pode estar matriculado em várias disciplinas
+        public virtual ICollection<Matricula> Matriculas { get; set; }
+
+        public Aluno()
+        {
+            Matriculas = new HashSet<Matricula>();
+        }
+    }
+}
---
+++ backend/Models/Matricula.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyUniversityAPI.Models
+{
+    public class Matricula
+    {
+        public int MatriculaId { get; set; }
+        public int AlunoId { get; set; }
+        public int DisciplinaId { get; set; }
+        public decimal Nota { get; set; }
+        public virtual Aluno Aluno { get; set; }
+        public virtual Disciplina Disciplina { get; set; }
+    }
+}
