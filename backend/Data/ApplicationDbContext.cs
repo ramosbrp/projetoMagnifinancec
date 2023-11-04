d1aca5f836d3d8ee9acff2fbba18c6599df29fc6
Bruno <bruno.rp45@gmail.com>
2023-11-04 19:08:21 -0300

    Banco de dados criado(Apenas para testar)

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MyUniversityAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyUniversityAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection") { }
+
+        public DbSet <Aluno> Alunos { get; set; }
+    }
+}
