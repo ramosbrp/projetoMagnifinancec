bb308f80a2d3342238c2661715331b39f3cfa3e8
Bruno <bruno.rp45@gmail.com>
2023-11-06 15:49:06 -0300

    Proxy

--- backend/Global.asax.cs
+++ backend/Global.asax.cs
@@ -18,20 +18,5 @@ namespace MyUniversityAPI
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-
-        protected void Application_BeginRequest()
-        {
-            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
-
-            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
-            {
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
-                HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
-                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
-                HttpContext.Current.Response.End();
-            }
-        }
     }
 }
