75cc3b9ff5ccd4b68f5299b5759e0fcfe98dd1bd
Bruno <bruno.rp45@gmail.com>
2024-06-21 17:15:58 -0300

    Connection string, ApplicationDbContext Adicionando pacotes EntityFramworkCore...

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+
+namespace MyUniversityAPP.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Defina DbSet<T> para cada entidade que você deseja incluir no seu modelo
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
