dcbd95b0980910f3f9e4c763e2e0aa28ce6e5925
Bruno <bruno.rp45@gmail.com>
2024-06-21 16:08:43 -0300

    DTOs

---
+++ backend/Models/DTO/ProfessorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyUniversityAPP.Models.DTO
+{
+    public class ProfessorDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public List<DisciplinaDto> Disciplinas { get; set; }
+    }
+}
